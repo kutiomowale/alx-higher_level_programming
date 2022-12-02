3c9578f8e06fbd0b54da1345ac8499dde6f84ea6
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-12-02 14:02:37 -0800

    Add 10-check_cycle.c and lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cycle - a function in C that checks
+ * if a singly linked list has a cycle in it
+ * @list: - pointer to list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *head;
+
+	head = list;
+	while (list->next != NULL)
+	{
+		if (list->next == head)
+			return (1);
+		list = list->next;
+	}
+	return (0);
+}
