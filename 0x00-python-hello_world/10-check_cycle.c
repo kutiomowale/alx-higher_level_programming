b03a2a3ddeb868b736c0a4b3ac35cc7ab0db928d
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-12-09 08:21:53 -0800

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,36 +2,33 @@
 /**
  * check_cycle - a function in C that checks
  * if a singly linked list has a cycle in it
+ * It does this by using two pointers
+ * one traverses the list one node at a time
+ * the other two nodes at a time
+ * Therefore the list has a cycle when the pointers meet at the same node
+ * When either of the pointers point to or get to a null pointer,
+ * it means the list has no cycle
  * @list: - pointer to list
  *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *nodes[1000];
-	int i, j, k;
+	listint_t *one_step, *two_step;
 
 	if (list == NULL)
 		return (0);
-	i = 0;
-	while (list->next != NULL)
+	one_step = two_step = list;
+	while (one_step && two_step)
 	{
-		nodes[i] = list;
-		j = 0;
-		k = i;
-		while (1)
-		{
-			if (list->next == nodes[j])
-				return (1);
-			if (list->next == nodes[k])
-				return (1);
-			if (k == (i / 2))
-				break;
-			j++;
-			k--;
-		}
-		list = list->next;
-		i++;
+		if (one_step->next == NULL)
+			break;
+		one_step = one_step->next;
+		if (two_step->next == NULL)
+			break;
+		two_step = two_step->next->next;
+		if (one_step == two_step)
+			return (1);
 	}
 	return (0);
 }
