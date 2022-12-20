d664b3cbd6ef7da17ecb9ddb2d8e2454005ca6a2
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-12-19 18:53:30 -0800

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    if value is None:
-        return False
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
