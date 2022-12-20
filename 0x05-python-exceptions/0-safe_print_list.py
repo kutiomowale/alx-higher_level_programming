bf8a1994f9bc7dbeb67ea11069f9308593d0f36e
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-12-19 18:28:07 -0800

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,13 @@
 
 def safe_print_list(my_list=[], x=0):
     if x is None:
+        print()
         return 0
     if x < 1:
+        print()
         return 0
     if my_list is None:
+        print()
         return 0
     n = 0
     for i in range(x):
