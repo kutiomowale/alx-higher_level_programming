f12e4590e48bab50a62198f1c39a30aca4199fd0
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-12-24 17:48:56 -0800

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This module contains a class Square that defines a square
+"""
+
+
+class Square:
+    """ This is a class that defines a square
+    """
+    def __init__(self, size):
+        """ Initializes the square
+        """
+        self.__size = size
