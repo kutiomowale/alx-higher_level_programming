e97ec88d1cfb5a2dc0f8a3eed5e080b4bceb35e2
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-01-10 19:36:04 -0800

    Add 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Number of keys
+
+A module that defines a function, that returns
+the number of keys in a dictionary.
+"""
+
+
+def number_keys(a_dictionary):
+    """Number of keys
+
+    A function that returns the number of keys in a dictionary
+
+    Args:
+        a_dictionary (dictionary): The dictionary
+
+    Returns:
+        The number of keys
+    """
+    # The number of keys or values in a dictionary is its length
+    return len(a_dictionary)
