fe648b9672e2f1656924e79efe9e07c0aaeddc0c
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-02-17 15:36:15 -0800

    Add 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write to a file
+Function definition for write_file"""
+
+
+def write_file(filename="", text=""):
+    """Write to a file
+
+    A function that writes a string to a text file (UTF8)
+    and returns the number of characters written
+    File permission exceptions are not managed
+    The file is created if doesn’t exist.
+    Contents of the file are overwritten if it already exists.
+
+    Args:
+        filename (str): Name of text file
+        text (str): The string
+
+    Returns:
+        The number of characters written
+
+"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
