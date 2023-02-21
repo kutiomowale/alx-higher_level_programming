997c52a278159c938478d645c59e9990d5879b16
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-02-21 09:12:26 -0800

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -42,21 +42,28 @@ try:
         i += 1
         """int: Line number starting from 1"""
         line = line.split()
-        """list: line text split by whitespace. i is line index from 0"""
-        if len(line) != 9:  # Input format has 8 whitespace characters
-            # so calling split on it creates a list of 9 elements
-            break
-        new_code = int(line[-2])
-        """int: Status code for a line"""
-        if new_code in appeared_codes:
-            appeared_codes[new_code] += 1
-        else:
-            appeared_codes[new_code] = 1
-        file_size = int((line[-1]).rstrip("\n"))
-        """int: File size for a line (sys.stin includes \n,
-        so it is removed)"""
-        total_file_size += file_size
-        if i % 10 == 0:
+        """list: line text split by whitespace. i is line number from 1"""
+        if len(line) >= 1:  # There should at least be a file size on a line
+            file_size = (line[-1]).rstrip("\n")
+            if file_size.isdigit():
+                file_size = int(file_size)
+                """int: File size for a line (sys.stin includes "\n",
+                so it is removed)"""
+                total_file_size += file_size
+
+            if len(line) >= 2:  # A valid status code might be present
+                new_code = line[-2]
+                if new_code.isdigit():
+                    new_code = int(line[-2])
+                    """int: Status code for a line"""
+                    if new_code in possible_codes:  # A valid code is present
+                        if new_code in appeared_codes:
+                            # Has it appeared before on the screen
+                            appeared_codes[new_code] += 1
+                        else:
+                            appeared_codes[new_code] = 1
+                            # If it has not appeared, include it
+        if i % 10 == 0:  # Print at line 10, 20, 30, ...
             print_stats(total_file_size, possible_codes, appeared_codes)
     print_stats(total_file_size, possible_codes, appeared_codes)
 except KeyboardInterrupt:
