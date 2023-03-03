b7f0152c9f12c43222bb9509d8c6e71af8079517
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-03-03 10:16:04 -0800

    Update models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,8 @@ class Rectangle(Base):
     def area(self):
         """Returns the area of a rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle instance with the character #"""
+        for i in range(self.__height):
+            print("#" * self.__width)
