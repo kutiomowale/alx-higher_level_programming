f1b6c52720ee638d135b0304b8131e31d55d5bf3
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-03-03 11:09:51 -0800

    Update models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,3 +86,8 @@ class Rectangle(Base):
         """prints in stdout the Rectangle instance with the character #"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        return "[Rectangle] ({}) {}/{} - {}/{}"
+        .format(self.id, self.__x, self.__y, self.__width, self.__height)
