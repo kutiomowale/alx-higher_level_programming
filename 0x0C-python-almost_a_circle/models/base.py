2b06c4f06674145dc1aa7275361330417f2b6b2e
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-03-06 08:15:08 -0800

    Update models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,3 +20,20 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries
+
+        Args:
+            list_dictionaries (list): Is a list of dictionaries
+
+        Returns:
+            If list_dictionaries is None or empty, return the string: "[]"
+            Otherwise, return the JSON string representation of
+            list_dictionaries
+        """
+        import json
+        if list_dictionaries and len(list_dictionaries) > 0:
+            return json.dumps(list_dictionaries)
+        return "[]"
