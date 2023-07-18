5d793b00d3769c0bdb810ffb96efe37954b3000f
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-07-18 14:02:17 -0700

    Add task 1 2 3 files

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- A script that creates a database hbtn_0c_0 in your MySQL server.
+-- It should not fail if hbtn_0c_0 already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- A script that deletes the database hbtn_0c_0 in your MySQL server.
+-- It should not fail if hbtn_0c_0 doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
