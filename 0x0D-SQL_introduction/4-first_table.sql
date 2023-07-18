83e696ef92a3b39d53fed918c81aaaeb4517ed8f
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-07-18 15:02:03 -0700

    Add task 4 and update task 3

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- A script that lists all the tables of a database in your MySQL server.
 -- The database name will be passed as argument of mysql command
-SHOW TABLES
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
