64b6ae796620833f1b963ecae85279bd0b4ac5c8
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-07-18 19:48:07 -0700

    Update task 14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -2,4 +2,4 @@
 -- in the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- The result column name should be average
 -- The database name will be passed as an argument of the mysql command
-SELECT AVG(score) AS AVERAGE FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
