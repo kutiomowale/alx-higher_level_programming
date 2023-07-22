f5dae0838aee731b751fbc507c1c156fbebb0613
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-07-21 18:21:10 -0700

    Update task 12 comment

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,6 @@
--- A script that lists all shows contained in hbtn_0d_tvshows
+-- A script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- If a show doesn’t have a genre, display NULL
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id
