27e8ca693e32336773db1241171e826157a89de4
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2023-08-16 05:22:31 -0700

    Update task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 function factorial (num) {
   if (Number.isNaN(num)) {
     return 1;
-  } else if (num == 0) {
+  } else if (num === 0) {
     return 1;
   } else {
     return num * factorial(num - 1);
